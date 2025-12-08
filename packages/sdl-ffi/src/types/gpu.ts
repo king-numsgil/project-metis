68feb7f28acf100263ac9fde2f2dd768173447a5
Daniel Grondin <king@numsgil.co>
2025-12-07 22:29:34 -0500

    Added a couple of enums in `gpu.ts` for working with buffers, but it's late...

--- packages/sdl-ffi/src/types/gpu.ts
+++ packages/sdl-ffi/src/types/gpu.ts
@@ -14,6 +14,88 @@ export enum ShaderFormat {
     MetalLib = 1 << 5,
 }
 
+export enum GPUVertexElementFormat {
+    INVALID,
+
+    // 32-bit Signed Integers
+    INT,
+    INT2,
+    INT3,
+    INT4,
+
+    // 32-bit Unsigned Integers
+    UINT,
+    UINT2,
+    UINT3,
+    UINT4,
+
+    // 32-bit Floats
+    FLOAT,
+    FLOAT2,
+    FLOAT3,
+    FLOAT4,
+
+    // 8-bit Signed Integers
+    BYTE2,
+    BYTE4,
+
+    // 8-bit Unsigned Integers
+    UBYTE2,
+    UBYTE4,
+
+    // 8-bit Signed Normalized
+    BYTE2_NORM,
+    BYTE4_NORM,
+
+    // 8-bit Unsigned Normalized
+    UBYTE2_NORM,
+    UBYTE4_NORM,
+
+    // 16-bit Signed Integers
+    SHORT2,
+    SHORT4,
+
+    // 16-bit Unsigned Integers
+    USHORT2,
+    USHORT4,
+
+    // 16-bit Signed Normalized
+    SHORT2_NORM,
+    SHORT4_NORM,
+
+    // 16-bit Unsigned Normalized
+    USHORT2_NORM,
+    USHORT4_NORM,
+
+    // 16-bit Floats
+    HALF2,
+    HALF4,
+}
+
+export enum GPUVertexInputRate {
+    Vertex,    // Attribute indexing uses the vertex index
+    Instance,   // Attribute indexing uses the instance index
+}
+
+export enum GpuBufferUsageFlags {
+    Vertex                = 1 << 0, // Vertex buffer
+    Index                 = 1 << 1, // Index buffer
+    Indirect              = 1 << 2, // Indirect buffer
+    GraphicsStorageRead   = 1 << 3, // Storage read in graphics stages
+    ComputeStorageRead    = 1 << 4, // Storage read in compute stage
+    ComputeStorageWrite   = 1 << 5, // Storage write in compute stage
+}
+
+export enum GpuTransferBufferUsage {
+    Upload,
+    Download,
+}
+
+export enum GpuShaderStage {
+    Vertex,
+    Fragment,
+}
+
 export enum GPULoadOp {
     Load,
     Clear,
