436b9a498667945908c65eaffbc40b7763934c3f
Daniel Grondin <king@numsgil.co>
2025-12-15 23:11:36 -0500

    DRAWING A FUCKING TRIANGLE!!!

--- packages/metis-app/src/main.ts
+++ packages/metis-app/src/main.ts
@@ -1,21 +1,33 @@
 import {
     Device,
     EventType,
+    GPUBlendFactor,
+    GPUBlendOp,
     GPUBufferUsageFlags,
+    GPUColorComponentFlags,
+    GPUCompareOp,
+    GPUCullMode,
+    GPUFillMode,
+    GPUFrontFace,
     GPULoadOp,
+    GPUPrimitiveType,
+    GPUSampleCount,
+    GPUShaderFormat,
+    GPUShaderStage,
+    GPUStencilOp,
     GPUStoreOp,
+    GPUTextureFormat,
     GPUTransferBufferUsage,
+    GPUVertexElementFormat,
+    GPUVertexInputRate,
     Keymod,
     Scancode,
-    GPUShaderFormat,
     System,
     Window
 } from "sdl3";
 import { sdlGetError } from "sdl3/ffi";
 
 import triangle from "./triangle.wgsl";
-
-console.log(triangle.vertex);
 
 function decodeKeymods(mod: Keymod): string[] {
     return (Object.keys(Keymod) as Array<keyof typeof Keymod>)
@@ -39,20 +51,54 @@ dev.claimWindow(wnd);
 console.log(`Device Driver : ${dev.driver}`);
 console.log(`Device Shader Format : ${GPUShaderFormat[dev.shader_formats]}`);
 
+using vertexShader = dev.createShader({
+    code: triangle.vertex!.spirv,
+    code_size: triangle.vertex!.spirv.length,
+    entrypoint: "vs_main",
+    format: GPUShaderFormat.SPIRV,
+    stage: GPUShaderStage.Vertex,
+    num_samplers: 0,
+    num_storage_buffers: 0,
+    num_storage_textures: 0,
+    num_uniform_buffers: 0,
+});
+
+using fragmentShader = dev.createShader({
+    code: triangle.fragment!.spirv,
+    code_size: triangle.fragment!.spirv.length,
+    entrypoint: "fs_main",
+    format: GPUShaderFormat.SPIRV,
+    stage: GPUShaderStage.Fragment,
+    num_samplers: 0,
+    num_storage_buffers: 0,
+    num_storage_textures: 0,
+    num_uniform_buffers: 0,
+});
+
+const bufferSize = 8 * Float32Array.BYTES_PER_ELEMENT * 3;
+const vertices = [
+    // x   y   z  w  r  g  b  a
+    [-.5, -.5, 0, 1, 1, 0, 0, 1],
+    [.5, -.5, 0, 1, 0, 1, 0, 1],
+    [.5, .5, 0, 1, 0, 0, 1, 1],
+]
+
 using buffer = dev.createBuffer({
     usage: GPUBufferUsageFlags.Vertex,
-    size: 3 * Float32Array.BYTES_PER_ELEMENT,
+    size: bufferSize,
 });
 {
     using transfer = dev.createTransferBuffer({
         usage: GPUTransferBufferUsage.Upload,
-        size: 3 * Float32Array.BYTES_PER_ELEMENT,
+        size: bufferSize,
     });
     transfer.map(array_buffer => {
         const array = new Float32Array(array_buffer);
-        array[0] = 1.0;
-        array[1] = 2.0;
-        array[2] = 3.0;
+        let i = 0;
+        for (const vertex of vertices) {
+            array.set(vertex, i);
+            i += 8;
+        }
     });
 
     const cb = dev.acquireCommandBuffer();
@@ -63,13 +109,101 @@ using buffer = dev.createBuffer({
     }, {
         buffer: buffer.raw,
         offset: 0,
-        size: 3 * Float32Array.BYTES_PER_ELEMENT,
+        size: bufferSize,
     });
     copy.end();
 
     using fence = cb.submitWithFence(dev);
     fence.wait();
 }
+
+using pipeline = dev.createGraphicsPipeline({
+    vertex_shader: vertexShader.raw,
+    fragment_shader: fragmentShader.raw,
+    vertex_input_state: {
+        num_vertex_buffers: 1,
+        vertex_buffer_descriptions: [
+            {
+                slot: 0,
+                pitch: 8 * Float32Array.BYTES_PER_ELEMENT,
+                input_rate: GPUVertexInputRate.Vertex,
+                instance_step_rate: 0,
+            },
+        ],
+        num_vertex_attributes: 2,
+        vertex_attributes: [
+            {
+                location: 0,
+                buffer_slot: 0,
+                format: GPUVertexElementFormat.FLOAT4,
+                offset: 0,
+            },
+            {
+                location: 1,
+                buffer_slot: 0,
+                format: GPUVertexElementFormat.FLOAT4,
+                offset: 4 * Float32Array.BYTES_PER_ELEMENT,
+            },
+        ],
+    },
+    rasterizer_state: {
+        fill_mode: GPUFillMode.Fill,
+        cull_mode: GPUCullMode.None,
+        front_face: GPUFrontFace.CounterClockwise,
+        depth_bias_clamp: 0,
+        depth_bias_constant_factor: 0,
+        depth_bias_slope_factor: 0,
+        enable_depth_bias: false,
+        enable_depth_clip: false,
+    },
+    target_info: {
+        num_color_targets: 1,
+        color_target_descriptions: [
+            {
+                format: dev.getSwapchinFormat(wnd),
+                blend_state: {
+                    alpha_blend_op: GPUBlendOp.Invalid,
+                    color_blend_op: GPUBlendOp.Invalid,
+                    color_write_mask: GPUColorComponentFlags.R | GPUColorComponentFlags.G | GPUColorComponentFlags.B | GPUColorComponentFlags.A,
+                    dst_alpha_blendfactor: GPUBlendFactor.Invalid,
+                    dst_color_blendfactor: GPUBlendFactor.Invalid,
+                    src_alpha_blendfactor: GPUBlendFactor.Invalid,
+                    src_color_blendfactor: GPUBlendFactor.Invalid,
+                    enable_blend: false,
+                    enable_color_write_mask: false,
+                },
+            },
+        ],
+        depth_stencil_format: GPUTextureFormat.Invalid,
+        has_depth_stencil_target: false,
+    },
+    depth_stencil_state: {
+        back_stencil_state: {
+            compare_op: GPUCompareOp.Invalid,
+            depth_fail_op: GPUStencilOp.Invalid,
+            fail_op: GPUStencilOp.Invalid,
+            pass_op: GPUStencilOp.Invalid,
+        },
+        compare_mask: 0,
+        compare_op: GPUCompareOp.Invalid,
+        enable_depth_test: false,
+        enable_depth_write: false,
+        front_stencil_state: {
+            pass_op: GPUStencilOp.Invalid,
+            fail_op: GPUStencilOp.Invalid,
+            depth_fail_op: GPUStencilOp.Invalid,
+            compare_op: GPUCompareOp.Invalid,
+        },
+        enable_stencil_test: false,
+        write_mask: 0,
+    },
+    multisample_state: {
+        sample_count: GPUSampleCount.One,
+        enable_mask: false,
+        sample_mask: 0,
+    },
+    primitive_type: GPUPrimitiveType.TriangleList,
+});
 
 let running = true;
 while (running) {
@@ -93,38 +227,19 @@ while (running) {
         load_op: GPULoadOp.Clear,
         store_op: GPUStoreOp.Store,
     }], null);
+    pass.bindGraphicsPipeline(pipeline);
+    pass.bindVertexBuffers([
+        {
+            buffer: buffer.raw,
+            offset: 0,
+        },
+    ]);
+    pass.drawPrimitives(3);
     pass.end();
 
     if (!cb.submit()) {
         console.log(`Failed to submit command buffer : ${sdlGetError()}`);
     }
-}
-
-{
-    using transfer = dev.createTransferBuffer({
-        usage: GPUTransferBufferUsage.Download,
-        size: 3 * Float32Array.BYTES_PER_ELEMENT,
-    });
-
-    const cb = dev.acquireCommandBuffer();
-    const copy = cb.beginCopyPass();
-    copy.downloadFromDeviceBuffer({
-        buffer: buffer.raw,
-        offset: 0,
-        size: 3 * Float32Array.BYTES_PER_ELEMENT,
-    }, {
-        transfer_buffer: transfer.raw,
-        offset: 0,
-    });
-    copy.end();
-
-    using fence = cb.submitWithFence(dev);
-    fence.wait();
-
-    transfer.map(array_buffer => {
-        const array = new Float32Array(array_buffer);
-        console.log(array);
-    });
 }
 
 dev.releaseWindow(wnd);
--- packages/metis-app/src/triangle.wgsl
+++ packages/metis-app/src/triangle.wgsl
@@ -1,17 +1,17 @@
 struct VertexOutput {
     @builtin(position) position: vec4f,
-    @location(0) color: vec3f,
+    @location(0) color: vec4f,
 }
 
 @vertex
-fn vs_main(@location(0) position: vec2f, @location(1) color: vec3f) -> VertexOutput {
+fn vs_main(@location(0) position: vec4f, @location(1) color: vec4f) -> VertexOutput {
     var output: VertexOutput;
-    output.position = vec4f(position, 0.0, 1.0);
+    output.position = vec4f(position.xy, 0.0, 1.0);
     output.color = color;
     return output;
 }
 
 @fragment
-fn fs_main(@location(0) color: vec3f) -> @location(0) vec4f {
-    return vec4f(color, 1.0);
+fn fs_main(@location(0) color: vec4f) -> @location(0) vec4f {
+    return vec4f(color.rgb, 1.0);
 }
