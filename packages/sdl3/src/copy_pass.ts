c5456d91445183cdf16f76290da5f4dbd1afca91
Daniel Grondin <king@numsgil.co>
2026-02-07 17:29:39 -0500

    Added wrappers for texture copy operations

--- packages/sdl3/src/copy_pass.ts
+++ packages/sdl3/src/copy_pass.ts
@@ -1,11 +1,20 @@
-import type { GPUBufferLocation, GPUBufferRegion, GPUTransferBufferLocation } from "sdl3";
+import type {
+    GPUBufferLocation,
+    GPUBufferRegion,
+    GPUTextureRegion,
+    GPUTextureTransferInfo,
+    GPUTransferBufferLocation,
+} from "sdl3";
 
 import {
     type GPUCopyPassPtr,
     sdlCopyGPUBufferToBuffer,
+    sdlCopyGPUTextureToTexture,
     sdlDownloadFromGPUBuffer,
+    sdlDownloadFromGPUTexture,
     sdlEndGPUCopyPass,
     sdlUploadToGPUBuffer,
+    sdlUploadToGPUTexture,
 } from "./ffi";
 
 export class CopyPass {
@@ -30,6 +39,22 @@ export class CopyPass {
     public copyBufferToBuffer(source: GPUBufferLocation, destination: GPUBufferLocation, size: number, cycle: boolean): void;
     public copyBufferToBuffer(source: GPUBufferLocation, destination: GPUBufferLocation, size: number, cycle?: boolean): void {
         sdlCopyGPUBufferToBuffer(this.handle, source, destination, size, cycle ?? false);
+    }
+
+    public uploadToTexture(source: GPUTextureTransferInfo, destination: GPUTextureRegion): void;
+    public uploadToTexture(source: GPUTextureTransferInfo, destination: GPUTextureRegion, cycle: boolean): void;
+    public uploadToTexture(source: GPUTextureTransferInfo, destination: GPUTextureRegion, cycle?: boolean): void {
+        sdlUploadToGPUTexture(this.handle, source, destination, cycle ?? false);
+    }
+
+    public downloadFromTexture(source: GPUTextureRegion, destination: GPUTextureTransferInfo): void {
+        sdlDownloadFromGPUTexture(this.handle, source, destination);
+    }
+
+    public copyTextureToTexture(source: GPUTextureTransferInfo, destination: GPUTextureTransferInfo, w: number, h: number, d: number): void;
+    public copyTextureToTexture(source: GPUTextureTransferInfo, destination: GPUTextureTransferInfo, w: number, h: number, d: number, cycle: boolean): void;
+    public copyTextureToTexture(source: GPUTextureTransferInfo, destination: GPUTextureTransferInfo, w: number, h: number, d: number, cycle?: boolean): void {
+        sdlCopyGPUTextureToTexture(this.handle, source, destination, w, h, d, cycle ?? false);
     }
 
     public end(): void {
